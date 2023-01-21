a493d52b769cb66913fc83cdd47bf2c37ff84b9b
MOHAN BABU S R <writeto.mohanbabu@gmail.com>
2023-01-21 12:59:11 +0530

    Angular material added
    ng add @angular/material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { Constants } from './app.constant';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: Constants.BASE_URL,
+    redirectTo: Constants.BASE_URL,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
